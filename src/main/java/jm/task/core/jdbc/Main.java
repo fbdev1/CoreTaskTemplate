38ecfcbe4bf792e03dd42d7f1fe0614e7f31ecbd
fbdev1 <syrkinvictor@gmail.com>
2021-04-15 18:46:49 +0500

    remove imports from main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,8 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
