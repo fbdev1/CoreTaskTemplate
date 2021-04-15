9afac21d54cc84998511d0d151738dd0db002352
fbdev1 <50766854+fbdev1@users.noreply.github.com>
2021-04-15 18:38:14 +0500

    delete target

--- target/test-classes/test.sql
+++
@@ -1,9 +0,0 @@
-#insert into users (name,lastName, age) values('Alex','Ivanov',55);
-
-#select * from users;
-#select * from users where name = 'Alex';
-#update users set name = 'Steve', age = 55 where id = 1;
-#delete from users;
-#delete from users where id = '14';
-#CREATE TABLE `mydbtest`.`users`(`id`INT NOT NULL AUTO_INCREMENT,`name`VARCHAR(45) NOT NULL,`age`INT(3) NULL,`lastName`VARCHAR(45) NULL,PRIMARY KEY(`id`))ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
-#drop table users123;
